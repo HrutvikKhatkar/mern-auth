321be91538f47cad88bfec95134a1b48f644975b
HrutvikKhatkar <hrutvikkhatkar25@gmail.com>
2024-09-13 17:28:39 +0530

    add redux toolkit

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,38 +1,50 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import {
+  signInStart,
+  signInSuccess,
+  signInFailure,
+} from "../redux/user/userSlice";
+import { useDispatch, useSelector } from "react-redux";
 
 export default function Signin() {
   const [formData, setFormData] = useState({});
-  const [error, setError] = useState(false);
-  const [loading, setLoading] = useState(false);
+  // const [error, setError] = useState(false);
+  // const [loading, setLoading] = useState(false);
+  const {loading, error} = useSelector((state) => state.user);
   const navigate = useNavigate();
+  const dispatch = useDispatch();
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value });
   };
   // console.log(formData);
   const handleSubmit = async (e) => {
     e.preventDefault();
-    try{
-      setLoading(true);
-      setError(false);
-      const res = await fetch('/api/auth/signin', {
-        method: 'POST',
+    try {
+      dispatch(signInStart());
+      // setLoading(true);
+      // setError(false);
+      const res = await fetch("/api/auth/signin", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
-        body: JSON.stringify(formData)
+        body: JSON.stringify(formData),
       });
       const data = await res.json();
       // console.log(data)
-      setLoading(false);
-      if(data.success == false){
-        setError(true);
+      // setLoading(false);
+      if (data.success == false) {
+        // setError(true);
+        dispatch(signInFailure(data));
         return;
       }
-      navigate('/')
-    }catch(err){
-      setLoading(false);
-      setError(true);
+      dispatch(signInSuccess(data));
+      navigate("/");
+    } catch (err) {
+      // setLoading(false);
+      // setError(true);
+      dispatch(signInFailure(error))
     }
     // console.log(data);
   };
@@ -55,8 +67,11 @@ export default function Signin() {
           id="password"
           onChange={handleChange}
         />
-        <button disabled={loading} className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
-          {loading ? 'Loading...' : 'Sign In'}
+        <button
+          disabled={loading}
+          className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
+        >
+          {loading ? "Loading..." : "Sign In"}
         </button>
       </form>
       <div className="flex gap-2 mt-5">
@@ -65,7 +80,7 @@ export default function Signin() {
           <span className="text-blue-500">Sign up</span>
         </Link>
       </div>
-      <p className="text-red-700 pt-5">{error && 'Something went wrong!'}</p>
+      <p className="text-red-700 pt-5">{error ? error.message || "Something went wrong!" : ""}</p>
     </div>
   );
 }
